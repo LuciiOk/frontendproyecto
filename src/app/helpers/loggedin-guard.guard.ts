4594fbc5d17ed8ad4f0caa25f7a79805d6307990
luciano portales <lportales655@gmail.com>
2021-11-20 03:28:54 -0300

    listo nav

--- src/app/helpers/loggedin-guard.guard.ts
+++ src/app/helpers/loggedin-guard.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { map, Observable, take } from 'rxjs';
 import { ConexionService } from '../service/conexion.service';
 
