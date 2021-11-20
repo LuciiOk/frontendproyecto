a8f466cfbf719af356bca88d146723e5edabd829
luciano portales <lportales655@gmail.com>
2021-11-20 15:06:34 -0300

    avance perfil, conexion con backend

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule, 
+    RouterModule
   ]
 })
 export class ProfileModule { }
